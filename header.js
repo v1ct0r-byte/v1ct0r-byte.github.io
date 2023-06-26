83961b500e39bfc29e1919a4cb05379a1b6af332
unknown <garciavicval@gmail.com>
2023-06-26 12:17:50 +0200

    UPDATED header style

--- header.js
+++ header.js
@@ -9,12 +9,12 @@ function create(htmlStr) {
 }
 
 // Create and insert image into header
-var img = create('<img src="images/UPV-Emblem2.png" width="22%">');
+var img = create('<img src="images/UPV-Emblem2.png" width="15%">');
 document.getElementById("header").insertBefore(img, document.getElementById("header").childNodes[0]);
 
 // Create and insert credentials into header
-var cred = create('<div><h1>Víctor García Valero</h1><p>Researcher at Universitat Politècnica de València</p></div>');
-document.getElementById("header").insertBefore(cred, document.getElementById("header").childNodes[1]);
+//var cred = create('<div><h1>Víctor García Valero</h1><p>Researcher at Universitat Politècnica de València</p></div>');
+//document.getElementById("header").insertBefore(cred, document.getElementById("header").childNodes[1]);
 
 // Create and insert navMenu into header
 var navMenu = create('<nav class="nav-menu"><ul><li><a href="index.html">Home</a></li><li><a href="projects.html">Projects</a></li><li><a href="publications.html">Publications</a></li><li><a href="contact.html">Contact</a></li></ul></nav>');
